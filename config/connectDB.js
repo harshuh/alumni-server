9901a6aa5385c7cc7e2bf7026c153a0efbf29b0c
Harsh <harshh1124@gmail.com>
2025-07-04 17:46:24 +0530

    mongoDB update

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
 
-//   Establishes a connection to MongoDB using mongoose.
-
+// Connects to MongoDB using environment variable URL
 export async function connectDatabase() {
   try {
-    await mongoose.connect(process.env.URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected successfully.");
+    await mongoose.connect(process.env.URL);
+    console.log("--> MongoDB connected successfully.");
   } catch (error) {
-    console.error(" MongoDB connection failed:", error.message);
-    process.exit(1); // Stop server if DB fails to connect
+    console.error("--> MongoDB connection failed:", error.message);
+    process.exit(1); // Exit app on DB connection failure
   }
 }
