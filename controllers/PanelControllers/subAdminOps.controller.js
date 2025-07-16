bedac8c8418494fe13882fecb00055fddbd387ba
Harsh <harshh1124@gmail.com>
2025-07-17 02:08:48 +0530

    alumni tab ke routes

--- controllers/PanelControllers/subAdminOps.controller.js
+++ controllers/PanelControllers/subAdminOps.controller.js
@@ -1,5 +1,7 @@
+import { Alumni } from "../../models/Alumni/alumniData.model.js";
 import { Subadmin } from "../../models/Moderator/subadmin.model.js";
 
+// subadmin TAB
 export const subAdminList = async (req, res) => {
   try {
     const subadmin = await Subadmin.find({}, { name: 1, username: 1 }).populate(
@@ -18,7 +20,6 @@ export const subAdminList = async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error });
   }
 };
-
 export const subAdminDelete = async (req, res) => {
   try {
     const username = (req.params.username || "").trim();
@@ -36,3 +37,33 @@ export const subAdminDelete = async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error });
   }
 };
+//
+
+// Alumni TAB
+export const getAlumni = async (req, res) => {
+  try {
+    const alumni = await Alumni.find({}, "-_id");
+    res.status(200).json(alumni);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error });
+  }
+};
+export const deleteAlumni = async (req, res) => {
+  try {
+    const enrollmentNo = (req.params.enrollmentNo || "").trim();
+    if (!enrollmentNo) {
+      return res.status(400).json({ message: "Enrollment Number Required!" });
+    }
+    const alumni = await Alumni.findByIdAndDelete({
+      enrollmentNo: enrollmentNo,
+    });
+    if (!alumni) {
+      return res.status(400).json({ message: "Alumni Not Found!!" });
+    }
+    return res.status(200).json({ message: "Alumni Deleted Successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error ", error: error });
+  }
+};
+//
--- routes/adminOperations/subAdminOps.route.js
+++ routes/adminOperations/subAdminOps.route.js
@@ -3,11 +3,18 @@ import { Router } from "express";
 import {
   subAdminList,
   subAdminDelete,
+  getAlumni,
+  deleteAlumni,
 } from "../../controllers/PanelControllers/subAdminOps.controller.js";
 
 const operationRouter = Router();
 
+// Subadmin Tab
 operationRouter.get("/view-subadmins", subAdminList);
 operationRouter.delete("/delete-subadmin/:username", subAdminDelete);
+
+// Alumni Tab
+operationRouter.get("/view-alumni", getAlumni);
+operationRouter.get("/delete-alumni/:enrollmentNo", deleteAlumni);
 
 export { operationRouter };
