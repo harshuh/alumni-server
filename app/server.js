7fe52e97abbe08b8d36520d0251029e25fb9b519
Harsh <harshh1124@gmail.com>
2025-07-20 19:41:11 +0530

    server.js change

--- app/server.js
+++ app/server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
     allowedHeaders: ["Authorization", "Content-Type", "x-access-token"],
