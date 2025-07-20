aa423e7602621ff090e79f4eef9d6750dd23ebc8
Harsh <harshh1124@gmail.com>
2025-07-20 19:31:55 +0530

    sameSite: http://localhost:5173

--- controllers/alumniControllers/alumni.controller.js
+++ controllers/alumniControllers/alumni.controller.js
@@ -109,7 +109,7 @@ export const loginAlumni = async (req, res) => {
 
     res.cookie("alumnitk", token, {
       httpOnly: true,
-      sameSite: "http:localhost:5173",
+      sameSite: "http://localhost:5173",
       secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000,
     });
