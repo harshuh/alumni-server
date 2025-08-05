786f4bb4f360bfcd52a015bf461e1b791bc029c8
Harsh <harshh1124@gmail.com>
2025-08-05 15:27:34 +0530

    this ??

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -4,6 +4,6 @@ import { adminAuth } from "../middlewares/adminAuth.js";
 
 export const checkRouter = express.Router();
 
-router.get("/check-auth", adminAuth, (req, res) => {
+checkRouter.get("/check-auth", adminAuth, (req, res) => {
   res.status(200).json({ message: "Authenticated ", adminId: req.adminId });
 });
