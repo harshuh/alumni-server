35df4ee2d2af5c87865bdd10c59551ca854a4653
Harsh <harshh1124@gmail.com>
2025-08-06 17:01:04 +0530

    lets try

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { adminAuth, subadminAuth } from "../middlewares/authMiddleware.js";
+
+import { adminAuth, subadminAuth } from "../middlewares/adminAuth.js";
 import { alumniAuth } from "../middlewares/alumniAuth.js";
 
 export const checkRouter = express.Router();
@@ -9,13 +10,11 @@ checkRouter.get("/admin-auth", adminAuth, (req, res) => {
     .status(200)
     .json({ message: "Admin authenticated", adminId: req.adminId });
 });
-
 checkRouter.get("/subadmin-auth", subadminAuth, (req, res) => {
   res
     .status(200)
     .json({ message: "Subadmin authenticated", subadminId: req.subadminId });
 });
-
 checkRouter.get("/alumni-auth", alumniAuth, (req, res) => {
   res
     .status(200)
