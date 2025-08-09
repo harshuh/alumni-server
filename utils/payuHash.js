c6e9c83237e344043093eded251c539697eee684
Harsh <harshh1124@gmail.com>
2025-08-09 21:12:46 +0530

    sdasdasda

--- utils/payuHash.js
+++ utils/payuHash.js
@@ -17,13 +17,17 @@ export const generateHash = async ({ email }, salt) => {
     firstname: user.alumniName,
     email: user.email,
     phone: user.phoneNo,
-    udf1: user.enrollmentNo,
+    udf1: udf1 || "",
+    udf2: udf2 || "",
+    udf3: udf3 || "",
+    udf4: udf4 || "",
+    udf5: udf5 || "",
 
     surl: "https://gbu-alumniserver.vercel.app/api/payu/pay/success", // success URL
     furl: "https://gbu-alumniserver.vercel.app/api/payu/pay/failure", // failure URL
   };
 
-  const hashString = `${params.key}|${params.txnid}|${params.amount}|${params.productinfo}|${params.firstname}|${params.email}|${params.phone}|${params.udf1}||||||||||${salt}`;
+  const hashString = `${params.key}|${params.txnid}|${params.amount}|${params.productinfo}|${params.firstname}|${params.email}|${params.phone}|${params.udf1}|${params.udf2}|${params.udf3}|${params.udf4}|${params.udf5}||||||${salt}`;
 
   const hash = crypto.createHash("sha512").update(hashString).digest("hex");
 
