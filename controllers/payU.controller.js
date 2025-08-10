1e466d632609ccdfbc29be1bd263cd9f85204720
Harsh <harshh1124@gmail.com>
2025-08-10 15:23:01 +0530

    added route in server.js

--- controllers/payU.controller.js
+++ controllers/payU.controller.js
@@ -1,4 +1,4 @@
-import { generateHash } from "../utils/payuHash.js";
+// import { generateHash } from "../utils/payuHash.js";
 
 export const paymentSuccess = (req, res) => {
   console.log("Payment success data:", req.params);
