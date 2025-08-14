e68bf89e372f0b8c1d3cfe87b1f6210754dafff8
Harsh <harshh1124@gmail.com>
2025-08-14 13:40:37 +0530

    Early Birds

--- utils/payuHash.js
+++ utils/payuHash.js
@@ -12,7 +12,7 @@ export const generateHash = async ({ email }, salt) => {
   const params = {
     key: process.env.PayU_MERCHENT_KEY,
     txnid: `txn_${Date.now()}`,
-    amount: "1000.00",
+    amount: "500.00",
     productinfo: "Alumni Membership",
     firstname: alumni.alumniName,
     email: alumni.email,
